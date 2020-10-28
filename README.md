2575f10af853f01fb2f0270d9c2fd9e2f230c3fc
Brian Glaws <glaws.brian@gmail.com>
2020-10-28 16:20:02 -0700

    Need to add Commands

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # DiscordBot
-its a bot
+Homework bot is a rudimentary chat bot I made for educational purposes. If you cheat, he will catch you. sike he just tells you what homework there is and when its due.
--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const client = new Discord.Client();
 const token = require("./token.js");
 
 client.on('ready', () => {
-	console.log('Logged in as ${client.user.tag}!');
+	console.log(`Logged in as ${client.user.tag}!`);
 });
-try {
-	client.login(token);
-} catch {
-	console.log("dernit");
-}  
+	
+client.on('message', msg => {
+	console.log(msg.content);
+	if (msg.content.startsWith("!")) {
+		msg.reply("I dont know what the homework is, sorry")
+	}
+});	
+
+client.login(token);
